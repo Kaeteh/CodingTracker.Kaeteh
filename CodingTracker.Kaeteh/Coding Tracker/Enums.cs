5d488515b00866d4483dbbbff991ecdceb365cb7
Kaeteh <90288749+Kaeteh@users.noreply.github.com>
2025-08-25 00:43:44 +0100

    .

---
+++ CodingTracker.Kaeteh/Coding Tracker/Enums.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using static Coding_Tracker.Enums;
+
+namespace Coding_Tracker
+{
+    internal class Enums
+    {
+        public enum MenuActions
+        {
+            ViewRecords,
+            AddRecords,
+            UpdateRecords,
+            DeleteRecords,
+            Exit          
+        }
+
+
+        internal enum TimeDuration
+        {
+            Start,
+            End
+        }
+    }
+}
